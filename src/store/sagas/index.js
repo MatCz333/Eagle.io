8a9d6efe980c958cd9ff336a3b7f4118f4b7fec2
Mateusz Czarnota <mateusz.czarnota@gmail.com>
2019-07-18 22:21:38 +1000

    cleaning repository

--- src/store/sagas/index.js
+++
@@ -1,7 +0,0 @@
-import { takeEvery } from "redux-saga/effects";
-import * as actionTypes from "../actions/actionTypes";
-import fetchDataSaga from "./dataInput";
-
-export default function* watchFetchData() {
-  yield takeEvery(actionTypes.FETCH_DATA_STARTED, fetchDataSaga);
-}
